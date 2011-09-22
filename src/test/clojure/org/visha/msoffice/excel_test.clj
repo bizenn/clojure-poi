b6cc2696252f8cab649d772ce9db2fe2e897acf3
Tatsuya BIZENN <bizenn@gmail.com>
2011-09-22 19:32:38 +0900

    Add tests.

--- src/test/clojure/org/visha/msoffice/excel_test.clj
+++ src/test/clojure/org/visha/msoffice/excel_test.clj
@@ -4,9 +4,16 @@
         [org.visha.msoffice.excel])
   (:import (org.apache.poi.ss.usermodel Workbook)))
 
-(deftest test_workbook-seq []
+(deftest test_xls-workbook-seq []
   (let [w (workbook (ClassLoader/getSystemResourceAsStream "sample.xls"))]
     (is (isa? (class w) Workbook))
-  ))
+    (is (= (class w) org.apache.poi.hssf.usermodel.HSSFWorkbook))
+    ))
+
+(deftest test_xlsx-workbook-seq []
+  (let [w (workbook (ClassLoader/getSystemResourceAsStream "sample.xlsx"))]
+    (is (isa? (class w) Workbook))
+    (is (= (class w) org.apache.poi.xssf.usermodel.XSSFWorkbook))
+    ))
 
 (run-tests)
