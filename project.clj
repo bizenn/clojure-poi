3281711c19f157618eb6b4e85dede8eaa2f07535
Tatsuya BIZENN <bizenn@gmail.com>
2011-11-09 17:14:57 +0900

    Fix dependencies.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject org.visha/clojure-poi "0.0.1-SNAPSHOT"
   :description "Simple wrapper for clojure using Apache POI"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.apache.poi/poi "3.8-beta4"]
+  :dependencies [[org.apache.poi/poi "3.8-beta4"]
                  [org.apache.poi/poi-ooxml "3.8-beta4"]]
-  :dev-dependencies [[swank-clojure "1.3.2"]]
+  :dev-dependencies [[org.clojure/clojure "1.3.0"]
+                     [swank-clojure "1.3.3"]]
   :source-path "src/main/clojure"
   :test-path "src/test/clojure"
   :resources-path "src/main/resources"
